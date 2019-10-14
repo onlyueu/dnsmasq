ae5b54b97b86d915467999aceb7534276e331ef4
onlyueu <55734657+onlyueu@users.noreply.github.com>
2019-10-14 09:40:10 +0800

    Create install_dnsmasq.sh

---
+++ install_dnsmasq.sh
@@ -0,0 +1,12 @@
+apt install dnsmasq
+
+sed -i -e "s/\#resolv\-file\=/resolv\-file\=\/etc\/resolv\.dnsmasq\.conf/" -e "s/\#strict\-order/strict\-order/" /etc/dnsmasq.conf
+echo "log-queries" >> /etc/dnsmasq.conf
+echo "log-facility=/var/log/dnsmasq.log" >> /etc/dnsmasq.conf
+echo "nameserver 1.1.1.1" > /etc/resolv.dnsmasq.conf 
+echo "nameserver 8.8.8.8" >> /etc/resolv.dnsmasq.conf 
+echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.dnsmasq.conf 
+sed -i '1inameserver 127.0.0.1' /etc/resolv.conf
+
+systemctl start dnsmasq
+systemctl enable dnsmasq

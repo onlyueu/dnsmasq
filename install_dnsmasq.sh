f803ccded9d59edc757cda3bc54a38ae588ddfc1
onlyueu <55734657+onlyueu@users.noreply.github.com>
2019-10-30 09:01:54 +0800

    Update install_dnsmasq.sh

--- install_dnsmasq.sh
+++ install_dnsmasq.sh
@@ -1,16 +1,16 @@
 sed -i -e "s/\#DNSStubListener\=yes/DNSStubListener\=no/" /etc/systemd/resolved.conf
-sudo systemctl disable systemd-resolved
-sudo systemctl stop systemd-resolved
+systemctl disable systemd-resolved
+systemctl stop systemd-resolved
+echo "prepend domain-name-servers 2606:4700:4700::1111;" >> /etc/dhcp/dhclient.conf
+echo "prepend domain-name-servers 8.8.8.8;" >> /etc/dhcp/dhclient.conf
+echo "prepend domain-name-servers 1.1.1.1;" >> /etc/dhcp/dhclient.conf
+echo "prepend domain-name-servers 127.0.0.1;" >> /etc/dhcp/dhclient.conf
 
-apt install dnsmasq
+apt install dnsmasq -y
 
 sed -i -e "s/\#resolv\-file\=/resolv\-file\=\/etc\/resolv\.dnsmasq\.conf/" -e "s/\#strict\-order/strict\-order/" /etc/dnsmasq.conf
 echo "log-queries" >> /etc/dnsmasq.conf
 echo "log-facility=/var/log/dnsmasq.log" >> /etc/dnsmasq.conf
-echo "nameserver 1.1.1.1" > /etc/resolv.dnsmasq.conf 
-echo "nameserver 8.8.8.8" >> /etc/resolv.dnsmasq.conf 
-echo "nameserver 2606:4700:4700::1111" >> /etc/resolv.dnsmasq.conf 
-sed -i '1inameserver 127.0.0.1' /etc/resolv.conf
 
 systemctl start dnsmasq
 systemctl enable dnsmasq
